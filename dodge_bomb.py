95c09bfe49b866654e63e030ca0726d5dee4ce8b
yuma <c0a231350a@edu.teu.ac.jp>
2024-10-01 15:16:44 +0900

    練習問題４

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -37,7 +37,8 @@ def main():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
-
+        if kk_rct.colliderect(bb_rct):
+            return
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         #if key_lst[pg.K_UP]:
