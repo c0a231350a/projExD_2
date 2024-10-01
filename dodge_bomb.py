b054208c21b6ad0f88bbc2d78452e091a8bcd654
yuma <c0a231350a@edu.teu.ac.jp>
2024-10-01 15:05:17 +0900

    練習問題３

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -8,6 +8,13 @@ WIDTH, HEIGHT = 1100, 650
 DELTA = {pg.K_UP: (0, -5),pg.K_DOWN: (0, +5),pg.K_LEFT: (-5, 0),pg.K_RIGHT: (+5, 0),}
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+def check_bound(obj_rct: pg.Rect) -> tuple[bool,bool]:
+    yoko, tate = True, True
+    if obj_rct.left < 0 or WIDTH < obj_rct.right:
+        yoko = False
+    if obj_rct.top < 0 or HEIGHT < obj_rct.bottom:
+        tate = False
+    return yoko, tate
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -46,8 +53,16 @@ def main():
                 sum_mv[0] += tpl[0]
                 sum_mv[1] += tpl[1]
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True,True):
+            kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
         screen.blit(kk_img, kk_rct)
+
         bb_rct.move_ip(vx,vy)
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
         screen.blit(bb_img,bb_rct)
         pg.display.update()
         tmr += 1
